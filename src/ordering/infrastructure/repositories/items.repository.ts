a4b36639f5190e6eb1df8ae5ac99609ad2a401b9
k-takahashi23 <efl.sad23@gmail.com>
2022-12-20 17:53:38 +0900

    ADD: RepositoryBase tmp

--- src/ordering/infrastructure/repositories/items.repository.ts
+++ src/ordering/infrastructure/repositories/items.repository.ts
@@ -3,27 +3,14 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Item } from '../../domain/aggregates/item/item.entity';
 import { ItemsRepositoryInterface } from 'src/ordering/domain/aggregates/item/items.repository.interface';
 import { Repository as TypeOrmRepository } from 'typeorm';
+import { RepositoryBase } from '../typeorm/repository.base';
 
 @Injectable()
-export class ItemsRepository implements ItemsRepositoryInterface {
-  @InjectRepository(Item)
-  private readonly _repository: TypeOrmRepository<Item>;
-
-  public async findAll(itemsArgs: any): Promise<Item[]> {
-    return this._repository.find();
-  }
-
-  public async findOneById(id: string): Promise<Item> {
-    return this._repository.findOneBy({ id })
-  }
-
-  public async save(entity: Item): Promise<void> {
-    this._repository.save(entity);
-    return;
-  }
-
-  public async remove(entity: Item): Promise<void> {
-    this._repository.remove(entity);
-    return;
+export class ItemsRepository extends RepositoryBase<Item> implements ItemsRepositoryInterface {
+  public constructor(
+    @InjectRepository(Item)
+    repository: TypeOrmRepository<Item>
+  ) {
+    super(repository);
   }
 }
---
+++ src/ordering/infrastructure/typeorm/repository.base.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Repository as TypeOrmRepository } from 'typeorm';
+import { Repository } from 'src/seedwork/repository';
+
+@Injectable()
+export abstract class RepositoryBase<TEntity> implements Repository<TEntity> {
+  private readonly _repository: TypeOrmRepository<TEntity>;
+
+  public constructor(repository: TypeOrmRepository<TEntity>) {
+    this._repository = repository;
+  }
+
+  public async findAll(itemsArgs: any): Promise<TEntity[]> {
+    return this._repository.find();
+  }
+
+  public async findOneById(id: string): Promise<TEntity> {
+    // TODO: id を渡すためとりあえずas any
+    return this._repository.findOneBy({ id } as any)
+  }
+
+  public async save(entity: TEntity): Promise<void> {
+    this._repository.save(entity);
+    return;
+  }
+
+  public async remove(entity: TEntity): Promise<void> {
+    this._repository.remove(entity);
+    return;
+  }
+}
